dd9f01daf5c83bae726ad051631b02ffd44add68
L. James Dowling <Lester.Dowling@gmail.com>
2023-07-28 07:08:59 +1000

    Merge env handling into OneAPI_TBB instead of separate module.

--- Packages/OneAPI_TBB.py
+++ Packages/OneAPI_TBB.py
@@ -2,10 +2,80 @@ import os
 import re
 import json
 import logging
+import subprocess
+from dotenv import load_dotenv
 from overrides import overrides
-from Packages.IPackage import IPackage
 from pathlib import Path
-import OneAPI_TBB_env
+from Packages.IPackage import IPackage
+
+def _create_env(env_path):
+    """
+    Create the environment variables to suit Intel's oneAPI.
+    Depends on the TBB_ROOT environment variable.
+    """
+    TBB_ROOT = Path(os.environ['TBB_ROOT'])
+    if not TBB_ROOT.exists():
+        raise Exception("No such directory: TBB Root")
+
+    with open(env_path, 'w') as fenv:
+        TBBVARS = TBB_ROOT / "env" /  "vars.bat"
+        if not TBBVARS.exists():
+            raise Exception("No such file: env/vars.bat")
+        os.environ['TBBVARS'] = str(TBBVARS)
+        fenv.write(f"TBBVARS={os.environ['TBBVARS']}\n")
+        p = subprocess.run([
+            "cmd.exe",
+            "/c",
+            "CALL",
+            str(TBBVARS),
+            "intel64",
+            "vs2022",
+            ">nul",
+            "2>&1",
+            "&&",
+            "set"
+        ], capture_output=True)
+        if p.returncode != 0:
+            raise Exception('cmd failed: ' + str(p))
+        m = str(os.getenv("PATH")).split(';')
+        z = list()
+        e = p.stdout.replace(b'\r\n', b'\n').decode('latin1')
+
+        for l in e.split('\n'):
+            s = l.split('=')
+            if len(s) == 2:
+                n, v = s
+                if not os.getenv(n):
+                    os.environ[n] = v
+                    fenv.write(f"{n}={v}\n")
+                    if n == 'PATH':
+                        for d in v.split(';'):
+                            d = d.replace('\\\\', '\\')
+                            if not d in m:
+                                m.append(d)
+                                z.append(d)
+        os.environ['PATH'] = ';'.join(z) + ';' + os.environ['PATH']
+        fenv.write(f"ZPATH={';'.join(z)}\n")
+
+
+def _setup_env():
+    """
+    Setup the environment variables for Intel's TBB.  Either: (1) read the
+    environment variable values from the cache file ~/.env, or (2) create anew the
+    environment variables, then cache them in env_path.
+    """
+    env_path = Path.home() / '.env-oneapi-tbb'
+    if env_path.exists():
+        p = os.environ['PATH']
+        load_dotenv(env_path)
+        if 'ZPATH' in os.environ.keys():
+            os.environ['PATH'] = os.environ['ZPATH'] + ';' + p
+        logging.debug(f'Loaded env from {env_path}')
+        logging.debug('PATH == {}'.format(os.environ['PATH']))
+    else:
+        _create_env(env_path)
+        logging.debug(f'Created env in {env_path}')
+        logging.debug('PATH == {}'.format(os.environ['PATH']))
 
 
 class TBB(IPackage):
@@ -15,7 +85,7 @@ class TBB(IPackage):
     """
 
     def __init__(self, argv : list[str] = []):
-        OneAPI_env.setup_env()
+        _setup_env()
         self._root = Path(os.environ['TBB_ROOT'])
         if not self._root.exists():
             raise Exception(f"No oneAPI TBB root: {self._root}")
